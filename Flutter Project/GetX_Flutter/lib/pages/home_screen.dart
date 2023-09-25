bbebe146f3dfdf010c464e16e9e5068259a84037
Harun Kusnaedi <harunkusnaedi@gmail.com>
2023-09-25 10:22:46 +0700

    initial commit

---
+++ Flutter Project/GetX_Flutter/lib/pages/home_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:project_flutter/controller/home_controller.dart';
+// import 'package:project_flutter/second_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    HomeController hc = Get.put(HomeController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Getx'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.bottomSheet(Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.grey,
+                    width: 100,
+                    height: 100,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextButton(
+                        onPressed: () {
+                          Get.toNamed("/indec"); // Navigasi ke halaman InDecScreen
+                        },
+                        child: Text("Increase/Decrease"),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.toNamed("/listsc"); // Navigasi ke halaman ListScreen
+                        },
+                        child: Text("List Screen"),
+                      ),
+                      ],
+                    )));
+              },
+              icon: const Icon(Icons.edit_note_outlined))
+        ],
+      ),
+      body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            children: [
+              TextField(
+                controller: hc.namacontroller,
+                decoration: const InputDecoration(labelText: 'Input Nama'),
+                onSubmitted: (value) {
+                  hc.gantiNilai(hc.namacontroller.text);
+                  // Get.defaultDialog(
+                  //     title: "nama yang di input",
+                  //     middleText: hc.namacontroller.text,
+                  //     onConfirm: () {
+                  //       Get.to(const SecondScreen(),
+                  //           arguments: hc.namacontroller.text);
+                  //     });
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // obx(()=> Text(hc.nama.value)),
+              GetBuilder<HomeController>(builder: (hc) {
+                return Text(hc.nama);
+              }),
+              Row(
+                children: [
+                  Obx(() => Text(
+                        hc.isOpen.value ? "Open" : "Close",
+                        style: TextStyle(
+                          color: hc.isOpen.value
+                              ? Colors.green
+                              : Colors
+                                  .red, // Ubah warna teks berdasarkan kondisi "Open" atau "Close"
+                        ),
+                      )),
+                  Obx(() => Switch(
+                        value: hc.isOpen.value,
+                        activeColor: hc.isOpen.value
+                            ? Colors.green
+                            : Colors
+                                .red, // Ubah warna berdasarkan kondisi "Open" atau "Close"
+                        onChanged: (value) {
+                          hc.setIsOpen(value);
+                        },
+                      ))
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
---
+++ Flutter Project/GetX_Flutter/lib/pages/inDec_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:project_flutter/controller/home_controller.dart';
+
+class InDecScreen extends StatelessWidget {
+  const InDecScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    HomeController hc = Get.find();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () {
+                  hc.increase();
+                },
+                icon: Icon(Icons.add)),
+            Obx(() => Text(
+                  "${hc.count.value}", // Menampilkan nilai dari HomeController
+                  style: TextStyle(fontSize: 24), // Gaya teks
+                )),
+            IconButton(
+                onPressed: () {
+                  hc.decrease();
+                },
+                icon: Icon(Icons.remove)),
+          ],
+        ),
+      ),
+    );
+  }
+}
